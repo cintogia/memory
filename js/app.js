f2c74c82d6c09185147f6c5c1213239168488f62
cintogia <50302747+cintogia@users.noreply.github.com>
2019-06-04 14:18:36 +0200

    feat: added modal
    * added modal to CSS/index.html from https://www.w3schools.com/howto/howto_css_modals.asp
    * added modal functionality in gameWon function from https://www.w3schools.com/howto/howto_css_modals.asp
    * retrieve result from score-panel / show result in modal
    * added "new game" button to modal

--- js/app.js
+++ js/app.js
@@ -2,17 +2,26 @@
  * Create a list that holds all of your cards
  */
 
-let cards = document.querySelectorAll('.card');
+let cards = document.querySelectorAll(".card");
 
-let card = ['./img/008.svg', './img/008.svg',
-    './img/007.svg', './img/007.svg',
-    './img/006.svg', './img/006.svg',
-    './img/005.svg', './img/005.svg',
-    './img/004.svg', './img/004.svg',
-    './img/003.svg', './img/003.svg',
-    './img/002.svg', './img/002.svg',
-    './img/001.svg', './img/001.svg',
-]
+let card = [
+  "./img/008.svg",
+  "./img/008.svg",
+  "./img/007.svg",
+  "./img/007.svg",
+  "./img/006.svg",
+  "./img/006.svg",
+  "./img/005.svg",
+  "./img/005.svg",
+  "./img/004.svg",
+  "./img/004.svg",
+  "./img/003.svg",
+  "./img/003.svg",
+  "./img/002.svg",
+  "./img/002.svg",
+  "./img/001.svg",
+  "./img/001.svg"
+];
 
 let openCards = [];
 
@@ -26,25 +35,26 @@ let openCards = [];
 shuffle(card);
 
 for (i = 0; i < cards.length; i++) {
-    const icons = document.createElement('img');
-    const icon = cards[i].appendChild(icons);
-    icon.setAttribute("src", card[i]);
+  const icons = document.createElement("img");
+  const icon = cards[i].appendChild(icons);
+  icon.setAttribute("src", card[i]);
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(cards) {
-    var currentIndex = cards.length,
-        temporaryValue, randomIndex;
+  var currentIndex = cards.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = cards[currentIndex];
-        cards[currentIndex] = cards[randomIndex];
-        cards[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = cards[currentIndex];
+    cards[currentIndex] = cards[randomIndex];
+    cards[randomIndex] = temporaryValue;
+  }
 
-    return cards;
+  return cards;
 }
 
 /*
@@ -58,110 +68,134 @@ function shuffle(cards) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-
 let total = 0;
 
 function countMoves(el) {
-    let moves = document.querySelector('.moves');
-    let star3 = document.querySelector('#star3');
-    let star2 = document.querySelector('#star2');
-    let star1 = document.querySelector('#star1');
-    // divide by two and round clicks to have: one Move = two open cards
-    total += el / 2;
-    moves.textContent = Math.round(total);
-    // star rating system
-    if (total > 8 && total <= 10) {
-        star3.classList.remove('fa-star');
-        star3.classList.add('fa-star-half-o');
-    } else if (total > 10 && total <= 12) {
-        star3.classList.remove('fa-star-half-o');
-        star3.classList.add('fa-star-o');
-    } else if (total > 12 && total <= 14) {
-        star2.classList.remove('fa-star');
-        star2.classList.add('fa-star-half-o');
-    } else if (total > 14 && total <= 16) {
-        star2.classList.remove('fa-star-half-o');
-        star2.classList.add('fa-star-o');
-    } else if (total > 16 && total <= 18) {
-        star1.classList.remove('fa-star');
-        star1.classList.add('fa-star-half-o');
-    } else if (total > 18) {
-        star1.classList.remove('fa-star-half-o');
-        star1.classList.add('fa-star-o');
-    }
+  let moves = document.querySelector(".moves");
+  let star3 = document.querySelector("#star3");
+  let star2 = document.querySelector("#star2");
+  let star1 = document.querySelector("#star1");
+  // divide by two and round clicks to have: one Move = two open cards
+  total += el / 2;
+  moves.textContent = Math.round(total);
+  // star rating system
+  if (total > 8 && total <= 10) {
+    star3.classList.remove("fa-star");
+    star3.classList.add("fa-star-half-o");
+  } else if (total > 10 && total <= 12) {
+    star3.classList.remove("fa-star-half-o");
+    star3.classList.add("fa-star-o");
+  } else if (total > 12 && total <= 14) {
+    star2.classList.remove("fa-star");
+    star2.classList.add("fa-star-half-o");
+  } else if (total > 14 && total <= 16) {
+    star2.classList.remove("fa-star-half-o");
+    star2.classList.add("fa-star-o");
+  } else if (total > 16 && total <= 18) {
+    star1.classList.remove("fa-star");
+    star1.classList.add("fa-star-half-o");
+  } else if (total > 18) {
+    star1.classList.remove("fa-star-half-o");
+    star1.classList.add("fa-star-o");
+  }
 }
 
 // event delegation
-const deck = document.querySelector('.deck');
-deck.addEventListener('click', onClick);
+const deck = document.querySelector(".deck");
+deck.addEventListener("click", onClick);
 
 function onClick(event) {
-    // makes Listener only valid for list items
-    if (event.target.nodeName === 'LI') {
-        event.target.classList.add('open', 'show');
-        openCards.push(event.target);
-        // prevent click on card twice
-        event.target.style.pointerEvents = "none";
-        // count every click on a card once and pass it to the appropriate function
-        countMoves(1);
-        // compare cards
-        if (openCards.length === 2 && openCards[0].firstElementChild.currentSrc ===
-            openCards[1].firstElementChild.currentSrc) {
-            addMatch();
-            console.log('true');
-            gameWon();
-        } else if (openCards.length === 2 && openCards[0].firstElementChild.currentSrc !== openCards[1].firstElementChild.currentSrc) {
-            misMatch();
-        }
+  // makes Listener only valid for list items
+  if (event.target.nodeName === "LI") {
+    event.target.classList.add("open", "show");
+    openCards.push(event.target);
+    // prevent click on card twice
+    event.target.style.pointerEvents = "none";
+    // count every click on a card once and pass it to the appropriate function
+    countMoves(1);
+    // compare cards
+    if (
+      openCards.length === 2 &&
+      openCards[0].firstElementChild.currentSrc ===
+        openCards[1].firstElementChild.currentSrc
+    ) {
+      addMatch();
+      console.log("true");
+      gameWon();
+    } else if (
+      openCards.length === 2 &&
+      openCards[0].firstElementChild.currentSrc !==
+        openCards[1].firstElementChild.currentSrc
+    ) {
+      misMatch();
     }
+  }
 }
 
 // lock cards and prevents clicks
 function addMatch() {
-    let cardOne = openCards[0];
-    let cardTwo = openCards[1];
-    cardOne.classList.remove('open', 'show');
-    cardOne.classList.add('match');
-    cardOne.style.pointerEvents = "none";
-    cardTwo.classList.remove('open', 'show');
-    cardTwo.classList.add('match');
-    cardTwo.style.pointerEvents = "none";
-    openCards = [];
+  let cardOne = openCards[0];
+  let cardTwo = openCards[1];
+  cardOne.classList.remove("open", "show");
+  cardOne.classList.add("match");
+  cardOne.style.pointerEvents = "none";
+  cardTwo.classList.remove("open", "show");
+  cardTwo.classList.add("match");
+  cardTwo.style.pointerEvents = "none";
+  openCards = [];
 }
 
 // show cards for one second and prevent clicks until cards are hidden again
 function misMatch() {
-    let cardOne = openCards[0];
-    let cardTwo = openCards[1];
-    deck.style.pointerEvents = "none";
-    setTimeout(() => { deck.removeAttribute("style") }, 1000);
-    setTimeout(() => {
-        cardOne.classList.remove('open', 'show');
-        cardOne.removeAttribute("style");
-        cardTwo.classList.remove('open', 'show');
-        cardTwo.removeAttribute("style");
-        openCards = [];
-    }, 1000);
+  let cardOne = openCards[0];
+  let cardTwo = openCards[1];
+  deck.style.pointerEvents = "none";
+  setTimeout(() => {
+    deck.removeAttribute("style");
+  }, 1000);
+  setTimeout(() => {
+    cardOne.classList.remove("open", "show");
+    cardOne.removeAttribute("style");
+    cardTwo.classList.remove("open", "show");
+    cardTwo.removeAttribute("style");
+    openCards = [];
+  }, 1000);
 }
 
 // show "New Game button" after winning the game
 // TODO: set timeout in order to show a few seconds after winning the game
 // TODO: remove reset Button from score-panel
 function gameWon() {
-    let matchedCards = document.querySelectorAll('.match');
-    if (matchedCards.length === 16) {
-        const page = document.querySelector('.container');
-        const result = document.querySelector('.score-panel');
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('container', 'newgame');
-        const newDiv1 = document.body.appendChild(newDiv);
-        newDiv1.appendChild(result);
-        page.style.display = "none";
-        newDiv1.insertAdjacentHTML('afterbegin', `<h1>You Won!</h1><button onClick="window.location.reload()">Start new game!</button>`);
-        console.log('you won!');
-    }
+  let matchedCards = document.querySelectorAll(".match");
+  if (matchedCards.length === 2) {
+    let modal = document.getElementById("myModal");
+
+    let moves = document.querySelector(".moves").textContent;
+    document.querySelector(".modal-body>p>span").textContent = moves;
+
+    let stars = document.querySelector("section>.stars").innerHTML;
+    document.querySelector("#result").innerHTML = stars;
+
+    // Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+    console.log("you won!");
+  }
 }
 // Reset button, reloads the page onClick
-document.querySelector('.restart').addEventListener('click', () => {
-    window.location.reload();
-})
+document.querySelector(".restart").addEventListener("click", () => {
+  window.location.reload();
+});
